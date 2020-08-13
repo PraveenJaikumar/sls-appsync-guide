23a8deb8c1be578bd98411d490a0102b9842cc71
PraveenJaikumar <45856026+PraveenJaikumar@users.noreply.github.com>
2020-08-13 23:20:58 +0530

    lg simple appsync

--- serverless-appsync-backend/scripts/deploy.bash
+++ serverless-appsync-backend/scripts/deploy.bash
@@ -3,10 +3,16 @@
 # Author: Serverless Guru
 
 stage=$1
+region=$2
 
 if [[ -z $stage ]];
 then
     stage="sandbox"
+fi
+
+if [[ -z $region ]];
+then
+    region="us-west-2"
 fi
 
 echo "**** Deploying to $stage ****"
@@ -14,7 +20,7 @@ echo "**** Deploying to $stage ****"
 SLS_DEBUG=*
 
 # Deploy additionalstacks first
-sls deploy additionalstacks --stage $stage -v
+sls deploy additionalstacks --stage $stage --region $region -v
 
 # Deploy main stack last
-sls deploy --stage $stage -v
+sls deploy --stage $stage --region $region -v
